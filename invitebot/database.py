9bea178b9e36c36d24ffb10c5991643f5f27297c
David Sn <divad.nnamtdeis@gmail.com>
2021-08-08 23:43:25 +0200

    Refactor Invite class and add helper functions
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- invitebot/database.py
+++ invitebot/database.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from invitebot import DB_URI
 
-from sqlalchemy import Column, Integer, Text, DateTime, func, create_engine
+from sqlalchemy import Column, Integer, Text, DateTime, func, create_engine, and_
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Query, sessionmaker
 
 # Configure a Session class.
 Session = sessionmaker()
@@ -29,8 +30,34 @@ class Invite(Base):
     link = Column(Text, unique=True, nullable=False)
     from_id = Column(Integer, nullable=False)
     from_name = Column(Text, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=func.now())
-    valid_until = Column(DateTime, nullable=False)
+    create_date = Column(DateTime, nullable=False, server_default=func.now())
+    expire_date = Column(DateTime, nullable=False)
     invitee_id = Column(Integer)
     invitee_name = Column(Text)
     joined_at = Column(DateTime)
+
+    def __init__(self, chat_id: int, link: str, from_id: int, from_name: str, create_date: datetime, expire_date: datetime):
+        self.chat_id = chat_id
+        self.link = link
+        self.from_id = from_id
+        self.from_name = from_name
+        self.create_date = create_date
+        self.expire_date = expire_date
+
+    def __repr__(self) -> str:
+        return f"<Invite (invite_id='{self.invite_id}', chat_id='{self.chat_id}', link='{self.link}')>"
+
+
+def query_invites_for_user(chat_id: int, user_id: int) -> Query:
+    return session.query(Invite).filter(
+        and_(
+            Invite.chat_id == chat_id,
+            Invite.from_id == user_id,
+            Invite.expire_date > func.now(),
+            Invite.invitee_id.is_(None)
+        )
+    ).order_by(Invite.invite_id)
+
+
+def create_tables() -> None:
+    Base.metadata.create_all(engine)
