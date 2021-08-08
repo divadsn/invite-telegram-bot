c2df9fb16580dca95f07598c91aa5c848c257cd1
David Sn <divad.nnamtdeis@gmail.com>
2021-08-09 00:42:24 +0200

    Add utils functions for Chat related things
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

---
+++ invitebot/utils.py
@@ -0,0 +1,50 @@
+from telegram import User, ChatMember, ChatMemberUpdated
+from typing import Tuple, Optional
+
+
+def extract_status_change(chat_member_update: ChatMemberUpdated) -> Optional[Tuple[bool, bool]]:
+    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
+    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
+    the status didn't change.
+    """
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get("is_member", (None, None))
+
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = (
+        old_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    )
+    is_member = (
+        new_status
+        in [
+            ChatMember.MEMBER,
+            ChatMember.CREATOR,
+            ChatMember.ADMINISTRATOR,
+        ]
+        or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+    )
+
+    return was_member, is_member
+
+
+def get_sender_name(sender: User) -> str:
+    """Returns the sender's username or full name if there is no username"""
+    if sender.username:
+        return "@" + sender.username
+    elif sender.first_name and sender.last_name:
+        return "{} {}".format(sender.first_name, sender.last_name)
+    elif sender.first_name:
+        return sender.first_name
+    elif sender.last_name:
+        return sender.last_name
+    else:
+        return "User:{}".format(sender.id)
